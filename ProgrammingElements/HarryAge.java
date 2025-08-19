b4e272397f49c9099758a500e29ae997a3be535d
Nishant489-r <nishant.prajapati_cs24@gla.ac.in>
2025-08-19 12:20:33 +0530

    HarryAge

---
+++ ProgrammingElements/HarryAge.java
@@ -0,0 +1,12 @@
+package ProgrammingElements;
+
+public class HarryAge {
+    public static void main(String[] args) {
+        int currentYear = 2024;
+        int birthYear = 2000;
+        int result = currentYear - birthYear;
+        System.out.println("Harry's age in 2024 is "+result);
+
+    }
+    
+}
