83ca33d905d4b0f70719ff8cb91104bd85088bea
Nishant489-r <nishant.prajapati_cs24@gla.ac.in>
2025-08-21 18:51:29 +0530

    KilomToMiles

---
+++ ProgrammingElements/Level1/KilomToMiles.java
@@ -0,0 +1,14 @@
+package ProgrammingElements.Level1;
+import java.util.Scanner;
+
+public class KilomToMiles {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Distance in km = ");
+        double Km = sc.nextDouble();
+        double miles =  Km * 0.621371;
+        System.out.println("Distance in miles = "+miles);
+        sc.close();
+    }
+    
+}
