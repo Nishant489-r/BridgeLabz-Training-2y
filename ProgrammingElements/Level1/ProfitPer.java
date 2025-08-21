2cea7f41a29197eb4fa0466dd06de8ca8c8f1d0f
Nishant489-r <nishant.prajapati_cs24@gla.ac.in>
2025-08-21 20:35:49 +0530

    ProfitPer

---
+++ ProgrammingElements/Level1/ProfitPer.java
@@ -0,0 +1,15 @@
+package ProgrammingElements.Level1;
+
+public class ProfitPer {
+    public static void main(String[] args) {
+        int costPrice = 129;
+        int sellprice = 191;
+        int profit = sellprice - costPrice;
+        double profitper = (profit/(double)costPrice)*100;
+        System.err.println("The Cost Price is "+ costPrice+ " and the selling price is "+sellprice);
+        System.out.println("The profit is "+profit+ " and the Profit percentage is "+String.format("%.2f",profitper)+"%");
+
+    }
+
+    
+}
