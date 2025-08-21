82a48e1933026950f5ada12efd91aa3a9645afc5
Nishant489-r <nishant.prajapati_cs24@gla.ac.in>
2025-08-21 20:35:14 +0530

    Pens

---
+++ ProgrammingElements/Level1/Pens.java
@@ -0,0 +1,16 @@
+package ProgrammingElements.Level1;
+
+public class Pens {
+    public static void main(String[] args) {
+        int TotalPens = 14;
+        int stu = 3;
+        int Pens = TotalPens / stu;
+        int remainPens = TotalPens%stu;
+        System.err.println("The pen per student is "+Pens+" and the remaining pen not distributed is "+ remainPens);
+
+
+
+    }
+
+    
+}
